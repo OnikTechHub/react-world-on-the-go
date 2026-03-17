ea1b68360852f38b75254376c3a2b41d3a9b18e8
onik-999 <onikdas.dev@gmail.com>
2026-03-17 21:42:20 +0600

    Third commit

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,8 +1,16 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from '../Country/Country';
 import './Countries.css'
 
 const Countries = ({ countriesPromise }) => {
+     const [visitedCountries, setVisitedCountries] = useState([]);
+     const handleVisitedCountries =(country) =>{
+        console.log('handle visited country cliucked', country);
+        const newVisitedCountries = [...visitedCountries, country]
+        setVisitedCountries(newVisitedCountries); 
+     }
+
+
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries;
 
@@ -11,11 +19,21 @@ const Countries = ({ countriesPromise }) => {
     return (
         <div>
             <h1>In the countries: {countries.length}</h1>
+            <h3>Total Country Visited:  {visitedCountries.length}</h3>
+            <ol>
+                {
+                    visitedCountries.map(country => <li>{country.name.common}</li>)
+                }
+            </ol>
             <div className='countries'>
                 {
                     countries.map(country => (<Country
                         key={country.cca3}
-                        country={country}></Country>
+                        country={country}
+                        handleVisitedCountries= {handleVisitedCountries}
+                        ></Country>
+                        
+                        
                     ))
                 }
             </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,21 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css'
 
-const Country = ( { country}) => {
-    // console.log(country.languages.languages)
+const Country = ( { country, handleVisitedCountries= {handleVisitedCountries}}) => {
+    
+    // console.log(country )
+
+    console.log(handleVisitedCountries)
+    const[visited, setVisited] = useState(false);
 
     const handleVisited = () =>{
-        console.log('btn click')
+
+        //----------------------- first system -----------------------------
+
+        // if(visited){
+        //     setVisited(false)
+        // }
+        // else{
+        //     setVisited(true)
+        // }
+
+        // ---------------------second system ---------------------------
+
+        // setVisited(visited ? false : true);
+
+        // -----------------third system ---------------------- 
+
+        setVisited(!visited);
+        handleVisitedCountries(country)
+
+
+
+
     }
     return (
-        <div className='country'>
+        
+        // <div className={ `country border-lg text-center ${visited ? 'country-visited' : 'country-not-visited'} `}> 
+        <div className={ `country ${visited && 'country-visited'} `}>
             <img src={country.flags.flags.png} alt={country.flags.flags.png} />
             <h3>Name: {country.name.common}</h3>
+            <p>Capital: {country.capital.capital}</p>
             <p>Population: {country.population.population}</p>
             {/* <p>languages: {languageNames}}</p> */}
             <p>Area: {country.area.area} { country.area.area > 300000 ? "Big Country " : 'Small country' }  </p>
+            <p>Region: {country.region.region}</p>
+            <p>Continents: {country.continents.continents}</p>
 
-            <button onClick={handleVisited}> Not Visited</button>
+            <button onClick={handleVisited}> 
+                {visited ? 'Visited' : 'Not Visited'}
+            </button>
         </div>
     );
 };
